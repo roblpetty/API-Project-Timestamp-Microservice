1aac4ca9411c78bc142f1f74e0653ab3debd7acd
Glitch (ffc-timestamp-microservice) <none>
2019-03-14 01:27:12 +0000

    Add api for no input

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // init project
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+let cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -15,6 +15,13 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+//api for case of blank date_string
+app.get("/api/timestamp/", (req, res) => {
+  let date = new Date()
+  res.json({"unix": date.getTime(), "utc": date.toUTCString()});
+});
+
+//api when date_string included
 app.get("/api/timestamp/:date_string", (req, res) => {
   
   let date = req.params.date_string;
